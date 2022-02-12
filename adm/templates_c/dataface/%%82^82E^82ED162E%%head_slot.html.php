af2fbd5a2b095d73a4c36961f729a7a223d63ea3
Ruben Debusscher <22316351+RubenDebusscher@users.noreply.github.com>
2022-02-12 17:58:18 +0100

    Delete adm/templates_c directory

--- adm/templates_c/dataface/%%82^82E^82ED162E%%head_slot.html.php
+++
@@ -1,2 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2020-12-21 15:16:28
-         compiled from head_slot.html */ ?>
--- adm/templates_c/g2/%%0B^0BA^0BAC1E95%%global_header.html.php
+++
@@ -1,2 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2020-12-21 15:16:35
-         compiled from global_header.html */ ?>
