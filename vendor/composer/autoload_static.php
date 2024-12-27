3b82becb954252df227aa6d0d8df086e62db3257
Ruben Debusscher <ruben.debusscher@doctorwhofans.be>
2024-12-27 22:32:22 +0100

    translations and plugins

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,12 +4,12 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit35be5a8db41bdec8256c7d75c7015528
+class ComposerStaticInit351b5634f0dd291509a02888b650174f
 {
     public static $files = array (
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -72,9 +72,9 @@ class ComposerStaticInit35be5a8db41bdec8256c7d75c7015528
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit35be5a8db41bdec8256c7d75c7015528::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit35be5a8db41bdec8256c7d75c7015528::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit35be5a8db41bdec8256c7d75c7015528::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit351b5634f0dd291509a02888b650174f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit351b5634f0dd291509a02888b650174f::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit351b5634f0dd291509a02888b650174f::$classMap;
 
         }, null, ClassLoader::class);
     }
